2d22d61d82da593677fd56f7f9ecc23cf98cfb73
Karl Cui <karl.cui@mail.utoronto.ca>
2019-12-03 00:00:04 -0500

    Create routes and link with header

--- website/src/app/app-routing.module.ts
+++ website/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AboutComponent } from './ui/pages/about/about.component';
+import { BlogComponent } from './ui/pages/blog/blog.component';
+import { ContactComponent } from './ui/pages/contact/contact.component';
+import { HomeComponent } from './ui/pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: HomeComponent},
+
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'blog', component: BlogComponent},
+    {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- website/src/app/app.component.spec.ts
+++ website/src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'website'`, () => {
+  it(`should have as title 'Karl Cui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('website');
+    expect(app.title).toEqual('Karl Cui');
   });
 
   it('should render title', () => {
