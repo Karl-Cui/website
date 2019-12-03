b3dae9d4bd19fa0cd7e394289f26ba52f4cce988
Karl Cui <karl.cui@mail.utoronto.ca>
2019-12-02 23:38:56 -0500

    Create page components

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './ui/header/header.component';
+import { HomeComponent } from './ui/pages/home/home.component';
+import { AboutComponent } from './ui/pages/about/about.component';
+import { BlogComponent } from './ui/pages/blog/blog.component';
+import { ContactComponent } from './ui/pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
