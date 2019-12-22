f6748e044151972f65988b2bca526187e96434ae
Karl Cui <karl.cui@mail.utoronto.ca>
2019-12-21 16:11:31 -0800

    Display blog posts on blog page

---
+++ website/src/app/ui/pages/blog/post-content.ts
@@ -0,0 +1,12 @@
+import { Post } from './post'
+
+export const POSTS: Post[] = [
+
+  {
+    date: "2019 Dec 21",
+    title: "First post",
+    description: "My first blog post!",
+    content: "Hello world"
+  }
+
+];
---
+++ website/src/app/ui/pages/blog/post.ts
@@ -0,0 +1,6 @@
+export class Post {
+  date: string;
+  title: string;
+  description: string;
+  content: string;
+}
